021e2a9bd1e0e8b3fd52f41b5c9d3c523506e44f
zze326 <zhangzhongen326@gmail.com>
2023-09-22 18:16:22 +0800

    wip: 清除无用代码

--- internal/model/mid/dept.go
+++ internal/model/mid/dept.go
@@ -1,10 +1,7 @@
 package mid
 
-import "github.com/gogf/gf/v2/os/gtime"
-
 type Dept struct {
-	Name      string      `v:"required" json:"name"`
-	Rank      int         `v:"required" json:"rank"`
-	ParentId  int         `json:"parentId"`
-	CreatedAt *gtime.Time `json:"createdAt"`
+	Name     string `v:"required" json:"name"`
+	Rank     int    `v:"required" json:"rank"`
+	ParentId int    `json:"parentId"`
 }
